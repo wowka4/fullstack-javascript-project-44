8e508835f5a6b3f13dd06840ccb1da294ea276b7
WoWka <vet-family@yandex.ru>
2023-10-13 13:53:43 +0300

    upd cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const userName = readlineSync.question('Welcome to the Brain Games!\nMay i have your name? ');
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May i have your name? ');
 
 export default userName; 
