2a5246b08b2324cd1e24ef50e4ba062859f6cceb
WoWka <vet-family@yandex.ru>
2023-10-13 17:44:44 +0300

    edit linters requirements

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -5,4 +5,5 @@ import userName from '../src/cli.js';
 
 // eslint-disable-next-line no-unused-expressions
 userName;
+// eslint-disable-next-line no-console
 console.log(`Hello, ${userName}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+// eslint-disable-next-line no-console
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May i have your name? ');
 
