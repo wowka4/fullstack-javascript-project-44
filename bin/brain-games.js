1a9d65e4f0b56dc986f0a04b9236216745951f9b
WoWka <vet-family@yandex.ru>
2023-10-13 12:41:19 +0300

    add greeting function

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import userName from "../src/cli.js";
+
+userName;
+console.log('Hello, ' + userName + '!');
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('Welcome to the Brain Games!\nMay i have your name? ');
+
+export default userName; 
