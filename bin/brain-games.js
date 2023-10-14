ad9a91831e368c06d0b023e58e8a9c71aa502510
WoWka <vet-family@yandex.ru>
2023-10-14 14:26:16 +0300

    make brain-even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions */
 
 // eslint-disable-next-line import/extensions
 import userName from '../src/cli.js';
+import isGameWin from './brain-even.js';
 
 // eslint-disable-next-line no-unused-expressions
 userName;
 // eslint-disable-next-line no-console
 console.log(`Hello, ${userName}!`);
+// eslint-disable-next-line no-console
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+isGameWin();
