ab1303bd2bdf925bd590231044ce7df29e22ee57
WoWka <vet-family@yandex.ru>
2023-10-27 17:02:40 +0300

    fix I

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,7 @@ import readlineSync from 'readline-sync';
 // eslint-disable-next-line no-console
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
 };
 
