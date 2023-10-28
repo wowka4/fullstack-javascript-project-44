77b6c8fb1cd60d1cfed05f50c1a7e6b992d9e7bd
WoWka <vet-family@yandex.ru>
2023-10-28 15:13:48 +0300

    fix bug brain-prime

--- games/brain-prime.js
+++ games/brain-prime.js
@@ -8,13 +8,13 @@ import { getRandomNumber } from '../src/randomNumber.js';
 import userName from '../src/userName.js';
 
 const isNaturalNumber = (num) => {
-  let count = 1;
+  let count = 0;
     for (let i = 2; i <= num / 2; i += 1) {
       if (num % i === 0) {
         count += 1;
       }
     }
-    return count < 2;
+    return count === 0;
   };
 
   const brainPrime = () => {
@@ -22,11 +22,9 @@ const isNaturalNumber = (num) => {
     const number = getRandomNumber(1, 1000);
     console.log(`Question: ${number} `);
     const userAnswer = readlineSync.question('Your answer: ');
-    let rightAnswer;
-    if (isNaturalNumber(number) === 'yes') {
+    let rightAnswer = 'no';
+    if (isNaturalNumber(number) === true) {
       rightAnswer = 'yes';
-    } else {
-        rightAnswer = 'no';
     }
     if (userAnswer !== rightAnswer) {
         console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.`);
