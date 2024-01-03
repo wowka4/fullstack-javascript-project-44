a9398dd9baac07037b6002de0e24f5e056dc657f
WoWka <vet-family@yandex.ru>
2024-01-03 17:04:45 +0300

    correct games/brain-gsd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,30 +3,16 @@ import runEngine from '../index.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
 
-const findMinOperator = (num1, num2) => {
-  if (num1 < num2) return num1;
-  return num2;
-};
-
-const findMax = (arr) => arr.reduce((prev, item) => {
-  if (prev < item) return item;
-  return prev;
-}, arr[0]);
-
-const rightAnswer = (num1, num2) => {
-  const arr = [];
-  for (let i = 1; i <= findMinOperator(num1, num2) / 2; i += 1) {
-    if (num1 % i === 0 && num2 % i === 0) arr.push(i);
-  }
-  const max = findMax(arr);
-  return max;
+const getGcd = (num1, num2) => {
+  if (num1 === 0) return num2;
+  return getGcd(num2 % num1, num1);
 };
 
 const generateRound = () => {
   const firstNumber = getRandomNumber(1, 100);
   const secondNumber = getRandomNumber(1, 100);
   const question = `${firstNumber} ${secondNumber}`;
-  const correctAnswer = String(rightAnswer(firstNumber, secondNumber));
+  const correctAnswer = String(getGcd(firstNumber, secondNumber));
   return [correctAnswer, question];
 };
 
